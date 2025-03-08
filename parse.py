50bf3def95c243289150534feaca12a55a363291
Jesse Carter <jesse.carter@gmail.com>
2025-03-08 11:33:58 -0500

    initial commit

---
+++ parse.py
@@ -0,0 +1,302 @@
+import requests
+from pprint import pprint
+
+def get_all_locations_in_region(region):
+    url = f'https://pinballmap.com/api/v1/region/{region}/locations.json'
+    try:
+        response = requests.get(url)
+        response.raise_for_status()  # Raise an error for bad status codes
+        locations = response.json()
+        
+        if len(locations) > 1:
+            raise ValueError(f'unknown key in response.json(), keys: {locations.keys()}')
+        return locations['locations']
+    except requests.exceptions.RequestException as e:
+        print(f"An error occurred: {e}")
+        return []
+
+def get_distance_in_miles(lat, long, mycoords=(40.380119, -80.044823)):
+    from geopy.distance import geodesic
+    coord1 = (lat, long)
+    return geodesic(coord1, mycoords).miles
+
+def get_distance_to_location(loc):
+    for location in locations:
+        if location['name'] == loc or location['id'] == loc:
+            return get_distance_in_miles(location['lat'], location['lon'])
+
+def get_machine_data():
+    url = f'https://pinballmap.com/api/v1/machines.json'
+    try:
+        response = requests.get(url)
+        response.raise_for_status()  # Raise an error for bad status codes
+        machines = response.json()
+        if len(machines) > 1:
+            raise ValueError(f'unknown key in machines.json(), keys: {machines.keys()}')
+        return machines['machines']
+    except requests.exceptions.RequestException as e:
+        print(f"An error occurred: {e}")
+        return []
+
+def get_machines_where_name_contains(s):    
+    machine_list = list()
+    for machine in machine_data:
+        if s in machine['name'].lower():
+            machine_list.append(machine)
+            pprint(f"{machine['name']} id: {machine['id']} group id: {machine['machine_group_id']}")
+    return machine_list
+
+def get_probable_group_id_where_name_contains(s):
+    machines = get_machines_where_name_contains(s)
+    # pprint(machines)
+    ids = [machine['machine_group_id'] for machine in machines]
+    return max(set(ids), key=ids.count)    
+
+def get_location_id(name):
+    for location in locations:
+        if name.lower() in location['name'].lower():
+            return location['id']
+
+def get_location_name(id):
+    for location in locations:
+        if id == location['id']:
+            return location['name']
+
+def remove_delimited_text(s, start='(', end=')'):
+    import re
+    pattern = re.escape(start) + r'.*?' + re.escape(end)
+    return re.sub(pattern, '', s)
+
+def get_machine_by_group_id(group_id):
+    for machine in machine_data:
+        if machine['machine_group_id'] == group_id:
+            return machine
+
+def get_machine_by_id(id):
+    for machine in machine_data:
+        if machine['id'] == id:
+            return machine
+
+def get_locations_with_machine_group_id(id):
+    locations_list = list()
+    for loc in locations:
+        for xref in loc['location_machine_xrefs']:
+            machine = xref['machine']
+            if machine['machine_group_id'] == id:
+                locations_list.append(loc)
+                break
+    return locations_list
+
+def get_locations_with_machine_id(id):
+    locations_list = list()
+    for loc in locations:
+        for xref in loc['location_machine_xrefs']:
+            machine = xref['machine']
+            if machine['id'] == id:
+                locations_list.append(loc)
+                break
+    return locations_list
+
+
+if __name__ == '__main__':
+    # get all locations in Pittsburgh
+    locations = get_all_locations_in_region('Pittsburgh')
+    print(f'Found {len(locations)} locations')
+
+    # get location ids for RBS tournament locations
+    tournament_locations = ['Lawrence Hall','Highline','Rival']
+    tournament_location_ids = list()    
+    for tloc in tournament_locations:
+        id = get_location_id(tloc)
+        name = get_location_name(id)
+        tournament_location_ids.append(id)
+        print(f"found {name}, id: {id}, distance: {get_distance_to_location(id):.1f} miles")
+        
+    # get machine data for ALL machines
+    machine_data = get_machine_data()
+
+    # get machine group ids for all machines at tournament locations
+    # machine group id is a way to identify several varients (Pro, Premium, ...) with one id
+    machine_group_ids = list()
+    location_dict = {loc['id']:loc for loc in locations}
+    for id in tournament_location_ids:
+        loc = location_dict[id]
+        print(loc['name'])
+        for xref in loc['location_machine_xrefs']:
+            machine = xref['machine']
+            # print(machine)
+            machine_group_ids.append(machine['machine_group_id'])
+            print(f"  {machine['name']}")
+            # pprint(loc)
+        print()
+
+    # some extra machines I'm interested in
+    extra_machines = ['sword of rage','madness','attack from mars','tales of the arabian']
+
+    # get locations for all machines identified above
+    machine_ids = list()
+    for name in extra_machines:
+        group_id = get_probable_group_id_where_name_contains(name)
+        if group_id:
+            machine_group_ids.append(group_id)
+        else:  # some older machines do not have group id, so just go off name and hope there is only one match
+            machines = get_machines_where_name_contains(name)
+            if len(machines) != 1:
+                print('could not get a single machine for name:',name,'matches:',machines)
+                continue
+            machine_ids.append(machines[0]['id'])
+
+
+    print(machine_group_ids)
+    print(machine_ids)
+
+    # start keeping track of which machines are at what locations
+    machine_location_dict = dict()
+
+    # loop over machine groups and get local locations which have each machine
+    for id in machine_group_ids:
+        name = remove_delimited_text(get_machine_by_group_id(id)['name']).strip()
+        if name not in machine_location_dict.keys():
+            machine_location_dict[name] = list()
+        print(name)
+        for loc in get_locations_with_machine_group_id(id):
+            miles = get_distance_in_miles(loc['lat'],loc['lon'])
+            if miles > 15:
+                continue
+            print(f"  {loc['name']} ({miles:.0f} miles)")
+            machine_location_dict[name].append(loc['name'].strip())
+
+    # again for machines without group id
+    for id in machine_ids:
+        name = get_machine_by_id(id)['name'].strip()
+        if name not in machine_location_dict.keys():
+            machine_location_dict[name] = list()
+        print(name)
+        for loc in get_locations_with_machine_id(id):
+            miles = get_distance_in_miles(loc['lat'],loc['lon'])
+            if miles > 15:
+                continue
+            print(f"  {loc['name']} ({miles:.0f} miles)")
+            machine_location_dict[name].append(loc['name'].strip())
+
+    pprint(machine_location_dict)
+
+    # compute True/False for each machine/location combination store into a pandas DataFrame
+    all_locations = sorted(set(loc for locs in machine_location_dict.values() for loc in locs))
+
+    import pandas as pd
+    new_df = pd.DataFrame(
+        {loc: [loc in locs for locs in machine_location_dict.values()] for loc in all_locations},
+        index=machine_location_dict.keys()
+    )
+    # new_df.to_json('new_data.json')
+
+   
+    # generate location rankings to show only most promising locations
+    ratings = pd.Series(index=new_df.columns)
+
+    for col in new_df.columns:
+        found_loc = False
+        for location in locations:
+            if location['name'] == col:
+                found_loc = True
+                distance = get_distance_in_miles(location['lat'],location['lon'])
+                break
+        if not found_loc:
+            print('*** could not find location', col)
+
+        machines = list(new_df[col].index[new_df[col].values])
+        num_machines = len(machines)
+        rating = num_machines/distance
+        ratings.loc[col] = rating
+
+        print(f"{col}, distance: {distance:.1f} miles, machines: {num_machines}, rating: {rating:.2f}")
+        for machine in machines:
+            print('  ',machine)
+    print(ratings.sort_values())
+
+    ranked_df = new_df[ratings[ratings>0.5].sort_values(ascending=False).index]
+    ranked_df.to_json('ranked.json')
+    print(ranked_df)
+
+    # import sys
+    # sys.exit(0)
+
+    # some fake data to show differences
+    # new_df['foo'] = False
+    # new_df.loc['Deadpool','foo'] = True
+    # new_df.loc['John Wick','Coop DeVille'] = False
+    # new_df.loc['JAWS','Cattivo'] = True
+    # new_df.loc['Jurassic Park','Cattivo'] = True
+    # new_df.drop(columns='Tiki Lounge', inplace=True)
+
+    print(new_df)
+
+    # load old data to look for differences
+    # df = pd.read_json('2025-03-05.json')  # eventually "old_data.json"
+    df = pd.read_json('old_data.json')
+
+    # **Find store changes BEFORE aligning**
+    old_stores = set(df.columns)
+    new_stores = set(new_df.columns)
+
+    added_stores = new_stores - old_stores
+    removed_stores = old_stores - new_stores
+    # print('added stores:', added_stores)
+    # print('removed stores:', removed_stores)
+
+    # **Align DataFrames (ensuring same structure)**
+    df_old, df_new = df.align(new_df, fill_value=False)
+
+    # **Find availability changes**
+    changes = df_old != df_new
+
+    # **Generate text-based differences**
+    change_log = list()
+    from datetime import datetime
+    date_string = datetime.now().strftime("%Y-%m-%d")
+
+    # Report new stores
+    for store in added_stores:
+        items_at_new_store = df_new.index[df_new[store]].tolist()
+        change_log.append({"date":date_string, "category":"add_location", "location":store, "machines":items_at_new_store})
+        for item in items_at_new_store:
+            print(f"A new location {store} is being tracked which has {item}.")
+
+    # Report removed stores
+    for store in removed_stores:
+        items_at_old_store = df_old.index[df_old[store]].tolist()
+        print(f"Location {store} is no longer tracked, which had: {', '.join(items_at_old_store)}.")
+        change_log.append({"date":date_string, "category":"remove_location", "location":store, "machines":items_at_old_store})
+
+    # process individual machine changes
+    import numpy as np
+    coord = np.where(changes)
+
+    # print('processing individual machine changes...')
+    for machine, location in [(df_new.index[x],df_new.columns[y]) for x, y in zip(coord[0], coord[1])]:
+        # print((machine, location))
+        if location in added_stores or location in removed_stores:
+            # print('  already handled', location)
+            continue
+        if df_new.at[machine, location]:  # Now available
+            print(f"{machine} is now available at {location}.")
+            change_log.append({"date":date_string, "category":"add_machine", "location":location, "machines":[machine]})
+        else:
+            print(f"{machine} is no longer available at {location}.")
+            change_log.append({"date":date_string, "category":"remove_machine", "location":location, "machines":[machine]})
+            
+    # **Print results**
+    # for change in changes_text:
+    #     print(change)
+    print()
+    print('change log:')
+    pprint(change_log)
+
+    # save changes as a DataFrame so we can export to CSV, append to previous changes
+    if len(change_log) > 0:
+        print(pd.DataFrame(change_log))
+        pd.DataFrame(change_log).to_csv('changelog.csv',index=False,header=False, mode='a')
+
+    # new data now becomes old
+    new_df.to_json('old_data.json')
---
+++ streamlit_app.py
@@ -0,0 +1,97 @@
+import streamlit as st
+import pandas as pd
+
+# Sample Data
+# data = {
+#     'Store A': {'Apples': True, 'Bananas': True, 'Carrots': False},
+#     'Store B': {'Apples': False, 'Bananas': True, 'Carrots': True},
+#     'Store C': {'Apples': True, 'Bananas': False, 'Carrots': True},
+# }
+
+# df = pd.DataFrame(data).T  # Stores as rows
+df = pd.read_json('ranked.json')
+
+# Function to style the DataFrame
+def style_table(df):
+    return df.style.applymap(lambda x: 'background-color: lightgreen' if x else 'background-color: lightcoral')
+
+st.title("Pinball Availability Checker")
+
+# Styled table display
+st.write("### Pinball Availability Overview")
+st.dataframe(style_table(df))
+
+# Location selection
+st.subheader("Filter by Location or Machine")
+machine_selected = st.selectbox("Select a machine", df.index)
+st.write(f"{machine_selected} available at:")
+for location in df.loc[machine_selected][df.loc[machine_selected]].index.tolist():
+    st.markdown(f" - {location}")
+# st.write('\n'.join(df.loc[machine_selected][df.loc[machine_selected]].index.tolist()))
+
+# Multi-item selection
+items_selected = st.multiselect("Select one or more machines", df.index.tolist())
+if items_selected:
+    filtered_locations = df.columns[df.loc[items_selected].all().tolist()]
+    st.write("Locations that have all selected machines:", filtered_locations)
+
+# Display changelog
+# read in log file and display all changes, newest first
+logdf = pd.read_csv('changelog.csv', names=['date','category','location','machines']).iloc[::-1]
+import ast
+for i in logdf.index.values:
+    row = logdf.iloc[i]
+    machines = ast.literal_eval(row['machines'])
+    for machine in machines:
+        print(f"date: {row['date']} category: {row['category']} location: {row['location']} machine: {machine}")
+
+category_colors = {
+    "add_location": "lightblue",
+    "add_machine": "lightgreen",
+    "remove_machine": "salmon",
+    "remove_location": "salmon"
+}
+category_descriptions = {
+    "add_location": "New location added",
+    "add_machine": "New machine added",
+    "remove_machine": "Machine removed",
+    "remove_location": "Location removed"
+}
+
+st.subheader("📜 Change Log (Latest Updates)")
+
+if logdf.empty:
+    st.info("No changes recorded yet.")
+else:
+    for _, row in logdf.iloc[0:200].iterrows():  # only show last 200 events
+        # set up block
+        category = row["category"]
+        color = category_colors.get(category, "gray")
+        markdown_string = f"<div style='background-color: {color}; padding: 10px; border-radius: 5px; margin-bottom: 5px;'>"
+        markdown_string += f"<b>{row['date']}</b> - <b>{category_descriptions[category]}</b>"
+        machines = ast.literal_eval(row['machines'])
+
+        # text description depends on category
+        if 'machine' in category:
+            for machine in machines:    
+                if category == 'add_machine':
+                    s = 'added at'
+                else:
+                    s = 'removed from'
+                markdown_string += f"<br>{machine} {s} {row['location']}"
+            
+            
+        elif 'location' in category:
+            if category == 'add_location':
+                for machine in machines:
+                    markdown_string += f"<br>New location added: {row['location']} which has {machine}"
+            else:
+                markdown_string += f"<br>{row['location']} no longer tracked, had machines: {', '.join(machines)}"
+        else:
+            markdown_string += f"unknown event {row}"
+
+        # finish up
+        markdown_string += "</div>"
+        st.markdown(markdown_string, unsafe_allow_html=True)
+                # <br>{machine}</div>",
+                
